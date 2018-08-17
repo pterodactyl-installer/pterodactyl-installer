268f26e2165b56409c482d00b6f33da0eb033c3c
Vilhelm Prytz <vilhelm@prytznet.se>
2018-08-17 17:05:15 +0200

    Restored apt dep command to original one for Ubuntu 18

--- install-panel.sh
+++ install-panel.sh
@@ -213,7 +213,7 @@ function ubuntu18_dep {
   apt update
 
   # Install Dependencies
-  apt -y install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-common php7.2-xml php7.2-curl mariadb-server nginx curl tar unzip git redis-server
+  apt -y install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl php7.2-zip mariadb-server nginx curl tar unzip git redis-server
 
   echo "* Dependencies for Ubuntu installed!"
 }
