a5b43f6d3e6252b120e1223465e1ef08177e7ce7
Vilhelm Prytz <vilhelm@prytznet.se>
2020-07-14 10:57:28 +0200

    daemon: minor tweaks
    fixed indentation, displaying of $virt_serv (remove \n) and minor code style tweaks

--- install-daemon.sh
+++ install-daemon.sh
@@ -115,15 +115,18 @@ function detect_distro {
 function check_os_comp {
   MACHINE_TYPE=$(uname -m)
   if [ "${MACHINE_TYPE}" != "x86_64" ]; then # check the architecture
-      print_warning "Detected architecture $MACHINE_TYPE"
-	   print_warning "Using any other architecture then 64 bit(x86_64) may (and will) cause problems."
-           echo -e -n  "* Are you sure you want to proceed? (y/N):"
-	   read -r choice
-      if [[ ! "$choice" =~ [Yy] ]]; then
-	    print_error "Installation aborted!"
-	    exit 1
-	  fi
+    print_warning "Detected architecture $MACHINE_TYPE"
+    print_warning "Using any other architecture then 64 bit(x86_64) may (and will) cause problems."
+
+    echo -e -n  "* Are you sure you want to proceed? (y/N):"
+    read -r choice
+
+    if [[ ! "$choice" =~ [Yy] ]]; then
+      print_error "Installation aborted!"
+      exit 1
+    fi
   fi
+
   if [ "$OS" == "ubuntu" ]; then
     if [ "$OS_VER_MAJOR" == "16" ]; then
       SUPPORTED=true
@@ -166,25 +169,24 @@ function check_os_comp {
     print_error "Unsupported OS"
     exit 1
   fi
+
+  # check virtualization
+  echo -e -n  "* Installing virt-what..."
   if [ "$OS" == "debian" ] || [ "$OS" == "ubuntu" ] || [ "$OS" == "zorin" ]; then
-    echo -e -n  "* Installing virt-what"
     apt-get -y update -qq > /dev/null
 
     # install virt-what
     apt-get install -y virt-what -qq > /dev/null
-	
-   elif [ "$OS" == "centos" ]; then
-	
+
+  elif [ "$OS" == "centos" ]; then
     if [ "$OS_VER_MAJOR" == "7" ]; then
-	  echo -e -n  "* Installing virt-what"
       yum -q -y update
 
       # install virt-what
       yum -q -y install virt-what
     elif [ "$OS_VER_MAJOR" == "8" ]; then
-	  echo -e -n  "* Installing virt-what"
       dnf -y -q update
-		
+
       # install virt-what
       dnf install -y -q virt-what
     fi
@@ -192,20 +194,21 @@ function check_os_comp {
     print_error "Invalid OS."
     exit 1
   fi
-  
+
   virt_serv=$(virt-what)
   if [ "$virt_serv" != "" ]; then
-      print_warning "Virtualization: $virt_serv detected."
+    print_warning "Virtualization: ${virt_serv//$'\n'/ } detected."
   fi
-  
+
   if [ "$virt_serv" == "openvz" ] || [ "$virt_serv" == "lxc" ] ; then # add more virtualization types which are not supported
-      print_warning "Unsupported type of virtualization detected. Please consult with your hosting provider whether your server can run Docker or not. Proceed at your own risk."
-      print_error "Installation aborted!"
-      exit 1
+    print_warning "Unsupported type of virtualization detected. Please consult with your hosting provider whether your server can run Docker or not. Proceed at your own risk."
+    print_error "Installation aborted!"
+    exit 1
   fi
+
   if uname -r | grep -q "xxxx"; then
-      print_error "Unsupported kernel detected."
-      exit 1
+    print_error "Unsupported kernel detected."
+    exit 1
   fi
 }
 
@@ -432,6 +435,7 @@ function main {
 
   # detect distro
   detect_distro
+
   # checks if the system is compatible with this installation script
   check_os_comp
 
