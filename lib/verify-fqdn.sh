bebd1879cb99b6afd74f1ee2240c25ab50265fb5
Vilhelm Prytz <vilhelm@prytznet.se>
2020-11-07 16:12:52 +0100

    Add a way to verify that a FQDN is valid

---
+++ lib/verify-fqdn.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -e
+
+#############################################################################
+#                                                                           #
+# Project 'pterodactyl-installer'                                           #
+#                                                                           #
+# Copyright (C) 2018 - 2020, Vilhelm Prytz, <vilhelm@prytznet.se>, et al.   #
+#                                                                           #
+#   This program is free software: you can redistribute it and/or modify    #
+#   it under the terms of the GNU General Public License as published by    #
+#   the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                     #
+#                                                                           #
+#   This program is distributed in the hope that it will be useful,         #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#   GNU General Public License for more details.                            #
+#                                                                           #
+#   You should have received a copy of the GNU General Public License       #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                           #
+# https://github.com/vilhelmprytz/pterodactyl-installer/blob/master/LICENSE #
+#                                                                           #
+# This script is not associated with the official Pterodactyl Project.      #
+# https://github.com/vilhelmprytz/pterodactyl-installer                     #
+#                                                                           #
+#############################################################################
+
+WORKING_DIR="/tmp/pterodactyl-verify-fqdn"
+
+# exit with error status code if user is not root
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+# check for python
+if ! [ -x "$(command -v python3)" ]; then
+  echo "* python3 is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+error() {
+  COLOR_RED='\033[0;31m'
+  COLOR_NC='\033[0m'
+
+  echo ""
+  echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
+  echo ""
+}
+
+webserver() {
+    rm -rf "$WORKING_DIR"
+    mkdir "$WORKING_DIR"
+    cd "$WORKING_DIR"
+
+    echo "pterodactyl-installer-verify-fqdn" > index.html
+
+    python3 -m http.server "$1" &
+
+    sleep 1
+
+    if [ "$(curl http://"$2":"$1")" != "pterodactyl-installer-verify-fqdn" ]; then
+        error "FQDN is not valid. See error message above for more info"
+        exit 1
+    fi
+
+    kill "$(jobs -p)"
+
+    rm -rf "$WORKING_DIR"
+}
+
+main() {
+    echo "Trying http://$1:80"
+    webserver "8080" "$1"
+
+    echo "Trying http://$1:443"
+    webserver "443" "$1"
+}
+
+main "$1"
