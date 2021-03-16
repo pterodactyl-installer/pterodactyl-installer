c3c57f60bdacae95620f9c35dc32081053528052
Vilhelm Prytz <vilhelm@prytznet.se>
2021-03-16 22:00:00 +0100

    Set version for development

--- install-wings.sh
+++ install-wings.sh
@@ -29,8 +29,8 @@ set -e
 #############################################################################
 
 # versioning
-GITHUB_SOURCE="v0.4.0"
-SCRIPT_RELEASE="v0.4.0"
+GITHUB_SOURCE="master"
+SCRIPT_RELEASE="canary"
 
 #################################
 ######## General checks #########
