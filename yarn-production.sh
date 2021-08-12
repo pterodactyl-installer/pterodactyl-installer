e5b38735404c1216e1d38db04fd17fb426272da3
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-08-12 17:49:00 -0400

    Update yarn-production.sh

--- yarn-production.sh
+++ yarn-production.sh
@@ -2,16 +2,15 @@
 
 #### Panel Production ####
 
-if [ -d "$var/www/pterodactyl" ]; then
-echo "The default directory exists, proceeding with the installation..."
+DIR=/var/www/pterodactyl
 
+if [ -d "$DIR" ]; then
+echo "The default directory exists, proceeding with the installation..."
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt-get install -y nodejs
 cd /var/www/pterodactyl
 npm i -g yarn
 yarn
-
 yarn build:production
-
 else
 echo "Default directory does not exist, aborting!"
 fi
