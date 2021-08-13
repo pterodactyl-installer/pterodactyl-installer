9a7f7cf3dd4ca7497b9b25203ccd4e2f3c245dff
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-08-13 17:00:21 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,25 @@
+#### Updating Repositories ####
+
+apt-get -y update
+apt-get -y upgrade
+
+#### Create MySQL User ####
+
+echo "Let's create a login user on the phpMyAdmin page."
+echo "Username: "
+read name
+if [ -z "$name" ]
+then
+echo "You have not entered a user!"
+else
+echo "Success!"
+fi
+
+echo "Password: "
+read password
+if [ -z "$password" ]
+then
+echo "You have not entered a password!"
+else
+echo "Success!"
+fi
