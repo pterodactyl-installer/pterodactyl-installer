9b83b98de05edf13055d25047db4d061bbb51bb3
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-08-13 14:29:13 -0400

    Create phpmyadmin_install.sh

---
+++ phpmyadmin_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#### Install PhpMyAdmin ####
+PHPMYADMIN=5.1.1
+DIR=/var/www/pterodactyl
+
+if [ -d "$DIR" ]; then
+echo "The default directory exists, proceeding with the installation..."
+cd /var/www/pterodactyl/public
+mkdir -p phpmyadmin
+cd phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/${PHPMYADMIN}/phpMyAdmin-${PHPMYADMIN}-all-languages.tar.gz
+tar -xzvf phpMyAdmin-${PHPMYADMIN}-all-languages.tar.gz
+cd phpMyAdmin-${PHPMYADMIN}-all-languages
+cp -R * /var/www/pterodactyl/public/phpmyadmin
+cd ..
+rm -R phpMyAdmin-${PHPMYADMIN}-all-languages phpMyAdmin-${PHPMYADMIN}-all-languages.tar.gz
+cp config.sample.inc.php config.inc.php
+rm -R config.sample.inc.php
+else
+echo "Default directory does not exist, aborting!"
+fi
