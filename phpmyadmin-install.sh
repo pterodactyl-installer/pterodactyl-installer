4026b34f60d18e6ec4a5753c72564c2567cfb9ea
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-08-13 19:20:55 -0400

    Update phpmyadmin-install.sh

--- phpmyadmin-install.sh
+++ phpmyadmin-install.sh
@@ -22,3 +22,44 @@ echo "Thanks for using this script, goodbye."
 else
 echo "Default directory does not exist, aborting!"
 fi
+
+#### Updating Repositories ####
+
+apt-get -y update
+apt-get -y upgrade
+
+#### Default MySQL credentials ####
+
+MYSQL_USER="admin"
+MYSQL_PASS="phpmyadminuser2021"
+
+#### Create MySQL User ####
+
+echo "Let's create a login user on the phpMyAdmin page."
+echo
+echo -n "* Username (default > admin): "
+read -r MYSQL_USER_INPUT
+[ -z "$MYSQL_USER_INPUT" ] && MYSQL_USER="admin" || MYSQL_USER=$MYSQL_USER_INPUT
+
+echo
+echo -n "* Password (default > phpmyadminuser2021)"
+read -r MYSQL_PASS_INPUT
+[ -z "$MYSQL_PASS_INPUT" ] && MYSQL_PASS="phpmyadminuser2021" || MYSQL_PASS=$MYSQL_PASS_INPUT
+echo "**************************************************"
+
+#### Database ####
+
+echo "* Creating user..."
+mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';"
+
+echo "* Creating database..."
+mysql -u root -e "CREATE DATABASE phpmyadmin;"
+
+echo "* Grant privileges..."
+mysql -u root -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;"
+
+echo "* Flush privileges..."
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+echo "* MySQL user created and configured successfully!"
+echo "**************************************************"
