189aefd48108deb1942989eaedd6955fa4cf1aac
Ferks <69549678+Ferks-FK@users.noreply.github.com>
2021-08-14 00:23:40 -0400

    Update phpmyadmin-install.sh

--- phpmyadmin-install.sh
+++ phpmyadmin-install.sh
@@ -41,9 +41,36 @@ read -r MYSQL_USER_INPUT
 [ -z "$MYSQL_USER_INPUT" ] && MYSQL_USER="admin" || MYSQL_USER=$MYSQL_USER_INPUT
 
 echo
+echo -n "* Database name (default > phpmyadmin): "
+read -r MYSQL_DATABASE_INPUT
+[ -z "$MYSQL_DATABASE_INPUT" ] && MYSQL_DATABASE="phpmyadmin" || MYSQL_DATABASE=$MYSQL_DATABASE_INPUT
+
+echo
 echo -n "* Password (default > phpmyadminuser2021)"
 read -r MYSQL_PASS_INPUT
 [ -z "$MYSQL_PASS_INPUT" ] && MYSQL_PASS="phpmyadminuser2021" || MYSQL_PASS=$MYSQL_PASS_INPUT
+
+#### Review of settings ####
+
+sumarry
+
+sumarry() {
+echo "* Username: $MYSQL_USER"
+echo "* Database name: $MYSQL_DATABASE"
+echo "* Password: $MYSQL_PASS"
+}
+
+echo -e -n "\n* Initial configuration completed. Continue with installation? (y/N): "
+read -r CONFIRM
+if [[ "$CONFIRM" =~ [Yy] ]]; then
+    continue_install
+  else
+    # run welcome script again
+    print_error "Installation aborted!"
+    exit 1
+fi
+
+continue_install() {
 echo "**************************************************"
 
 #### Database ####
@@ -52,10 +79,10 @@ echo "* Creating user..."
 mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASS}';"
 
 echo "* Creating database..."
-mysql -u root -e "CREATE DATABASE phpmyadmin;"
+mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
 
 echo "* Grant privileges..."
-mysql -u root -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION;"
 
 echo "* Flush privileges..."
 mysql -u root -e "FLUSH PRIVILEGES;"
@@ -69,4 +96,4 @@ echo "* MySQL user created and configured successfully!"
 echo
 echo "* Thanks for using this script, goodbye."
 echo "**************************************************"
-
+}
