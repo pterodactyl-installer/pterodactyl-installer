8a26554a5ef4380a94fcf757d4c068994d45ca41
Vilhelm Prytz <vilhelm@prytznet.se>
2022-01-06 15:06:55 +0100

    wings: remove unnecessary surrounding quotes

--- install-wings.sh
+++ install-wings.sh
@@ -367,8 +367,8 @@ install_docker() {
 
     # Add docker repo
     echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/"$OS" \
-      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/$OS \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
     # Install docker
     apt_update
