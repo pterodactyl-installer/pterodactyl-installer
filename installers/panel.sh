d9b89c57f86f104e1fd7fcbf30cdb01db6a1a568
Linux123123 <me@linux123123.com>
2022-10-07 20:56:42 +0300

    Change wget for curl

--- installers/panel.sh
+++ installers/panel.sh
@@ -261,7 +261,7 @@ debian_dep() {
   install_packages "dirmngr ca-certificates apt-transport-https lsb-release"
 
   # Install PHP 8.1 using sury's repo
-  wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+  curl -o /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
   echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
 }
 
