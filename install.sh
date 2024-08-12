5bf8df7f552a78a67c0b2bd09cf0cc0bae78aa00
Caydenn <103391432+QXIoa@users.noreply.github.com>
2024-08-12 11:20:51 -0400

    Made enhancements (#502)

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ if ! [ -x "$(command -v curl)" ]; then
 fi
 
 # Always remove lib.sh, before downloading it
-rm -rf /tmp/lib.sh
+[ -f /tmp/lib.sh ] && rm -rf /tmp/lib.sh
 curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/master/lib/lib.sh
 # shellcheck source=lib/lib.sh
 source /tmp/lib.sh
