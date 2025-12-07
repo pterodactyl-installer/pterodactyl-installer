ed688e1947c906f3a432c2f9c7ea69d98f6324fc
yopi-def <yopiahmadafrizal08@gmail.com>
2025-12-07 12:29:08 +0700

    Update lib.sh

--- lib/lib.sh
+++ lib/lib.sh
@@ -49,7 +49,7 @@ export ARCH=""
 export SUPPORTED=false
 
 # download URLs
-export PANEL_DL_URL="https://github.com/yopi-def/panel/releases/latest/download/panel.tar.gz"
+export PANEL_DL_URL="https://github.com/yopi-def/panel/archive/refs/heads/master.zip"
 export WINGS_DL_BASE_URL="https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_"
 export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
 export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/yopi-def/pterodactyl-installer"}
